531480fe957864ad8127a20d8333069a990e589f
NSIX06 <felipebugalho2016@gmail.com>
2023-09-29 09:45:34 -0300

    top

---
+++ js/top.js
@@ -0,0 +1,17 @@
+function mudarTema()
+{
+   var  d = document.body.classList.toggle("escuro");
+    var l = document.getElementById("btn");
+
+    if( d == true ) 
+    {
+        l.innerHTML = "light_mode";
+    }
+    else
+    {
+
+        l.innerHTML = "dark_mode";
+    }
+
+
+}
