70bddbc78803005af211379d8f43cb3053380f06
NSIX06 <felipebugalho2016@gmail.com>
2023-09-29 11:52:49 -0300

    top3

--- js/top.js
+++ js/top.js
@@ -1,17 +1,35 @@
+
+
 function mudarTema()
+
 {
+
    var  d = document.body.classList.toggle("escuro");
+
     var l = document.getElementById("btn");
 
+ 
+
     if( d == true ) 
+
     {
+
         l.innerHTML = "light_mode";
+
     }
+
     else
+
     {
+
+ 
 
         l.innerHTML = "dark_mode";
+
     }
 
+ 
+
+ 
 
 }
